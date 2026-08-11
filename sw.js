93bc379ef2c3b014b3ddbb6c8cda6bb5b7caba75
clem989 <clementgom@protonmail.com>
2026-08-12 00:30:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fcl-v26';
+const CACHE='fcl-v27';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png'])).catch(()=>{}));});
 self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
 self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(self.clients.matchAll({type:'window'}).then(cl=>{for(const c of cl){if('focus' in c)return c.focus();}if(self.clients.openWindow)return self.clients.openWindow('./');}));});
